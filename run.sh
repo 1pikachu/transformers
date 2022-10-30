a12c7552fc666e142641d1edbabe8c5693f7520b
dujun <jun.du@intel.com>
2022-10-30 02:42:39 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 pip install evaluate datasets
+pip uninstall transformers
 pip install -e .
 
 python examples/tensorflow/language-modeling/run_mlm.py --model_name_or_path bert-base-multilingual-uncased --output_dir /tmp/tmp0 --dataset_name wikitext  --dataset_config_name wikitext-103-raw-v1 --epochs 3 --num_iter 100 --num_warmup 1 --precision float16 --per_device_eval_batch_size 16
