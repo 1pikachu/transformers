a5cbd9aaa1b64c0ac747c169fd0ed9e144f2e4ff
dujun <jun.du@intel.com>
2022-11-14 02:23:30 +0000

    run.sh add note

--- run.sh
+++ run.sh
@@ -4,3 +4,8 @@ python setup.py develop
 
 
 python ./examples/pytorch/text-classification/run_glue.py --task_name MRPC --model_name_or_path allenai/longformer-base-4096  --do_eval --no_cuda --overwrite_output_dir --output_dir /tmp/tmp0 --per_device_eval_batch_size 1 --num_iters 200 --num_warmup 20 --channels_last 1 --precision float16  --device cuda --profile
+
+
+# if run xpu fail with "ValueError: not enough values to unpack (expected 2, got 1)"
+# should remove --jit
+python ./examples/pytorch/text-classification/run_glue.py  --task_name  MRPC  --model_name_or_path  allenai/longformer-base-4096  --do_eval  --no_cuda  --overwrite_output_dir  --output_dir  /tmp/tmp0  --per_device_eval_batch_size  1  --num_iter  200  --num_warmup  20  --channels_last  1  --precision  float16  --device  xpu --jit
